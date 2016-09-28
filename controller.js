97254139297b2b75d7a3df07a292b27a1cb0a33a
Michael Hanks <mikeHanks@Michaels-MacBook-Pro.local>
2016-09-28 12:32:45 -0600

    finished

--- controller.js
+++ controller.js
@@ -1,10 +1,9 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
-
   $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
+    mainService.getUsers().then(function(response) {
+      console.log(response.data.data)
+      $scope.users = response.data.data;
+    });
   }
-
-
   $scope.getUsers();
-
 });
